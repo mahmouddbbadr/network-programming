a6b5baf96802c3d5751063c8811d0df058189200
mahmoud badr abdelfattah <120060696+mahmouddbbadr@users.noreply.github.com>
2024-05-12 23:17:59 +0300

    Update TIC-Tac-Toe.py

--- TIC-Tac-Toe.py
+++ TIC-Tac-Toe.py
@@ -1,3 +1,50 @@
+from tkinter import *
+from tkinter import messagebox
+window=Tk()
+
+window.title("Welcome to The Gaming world TIC-Tac-Toe ")
+window.geometry("400x300")
+
+lbl=Label(window,text="Tic-tac-toe Game",font=('Helvetica','15'))
+lbl.grid(row=0,column=0)
+lbl=Label(window,text="Player 1: X",font=('Helvetica','10'))
+lbl.grid(row=1,column=0)
+lbl=Label(window,text="Player 2: O",font=('Helvetica','10'))
+lbl.grid(row=2,column=0)
+
+turn=1; #For first person turn.
+def reset():
+    global flag, turn
+    flag = 1
+    turn = 1
+    btn1["text"] = " "
+    btn2["text"] = " "
+    btn3["text"] = " "
+    btn4["text"] = " "
+    btn5["text"] = " "
+    btn6["text"] = " "
+    btn7["text"] = " "
+    btn8["text"] = " "
+    btn9["text"] = " "
+def clicked1():
+    global turn
+    if btn1["text"]==" ":   #For getting the text of a button
+        if turn==1:
+            turn =2;
+            btn1["text"]="X"
+        elif turn==2:
+            turn=1;
+            btn1["text"]="O"
+        check();
+def clicked2():
+    global turn
+    if btn2["text"]==" ":
+        if turn==1:
+            turn =2;
+            btn2["text"]="X"
+        elif turn==2:
+            turn=1;
+            btn2["text"]="O"
         check();
 def clicked3():
     global turn
